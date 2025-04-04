48dbbb8c9cd24511bb55f20fe6677bf24d3bee7b
Manan Bhimjiyani <mananbhimjiyani@gmail.com>
2025-04-05 02:56:08 +0530

    Updated CI/CD automation

--- stock_forex_app/settings.py
+++ stock_forex_app/settings.py
@@ -145,12 +145,31 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Define the directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Define the URL prefix for static files
+STATIC_URL = '/static/'
+
+# Optional: Additional directories for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app', 'static'),
+]
+
+INSTALLED_APPS += ['storages']
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'stock-forex-app'
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Use S3 for static files
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,38 @@
-# terraform/main.tf
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "manan" # Use the 'manan' profile
 }
 
 # S3 Bucket for static files
 resource "aws_s3_bucket" "static_files" {
-  bucket = "your-s3-bucket-name"
-  acl    = "public-read"
+  bucket = "stock-forex-app"
 
-  website {
-    index_document = "index.html"
+  # Enable Object Ownership (BucketOwnerEnforced)
+  object_lock_enabled = false
+}
+
+# Configure the S3 bucket as a static website
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_files.bucket
+
+  index_document {
+    suffix = "index.html"
   }
+}
+
+# CloudFront Origin Access Identity (OAI)
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for stock-forex-app"
 }
 
 # CloudFront Distribution for CDN
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
     domain_name = aws_s3_bucket.static_files.bucket_regional_domain_name
-    origin_id   = "S3-your-s3-bucket-name"
+    origin_id   = "S3-stock-forex-app"
 
     s3_origin_config {
-      origin_access_identity = ""
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
   }
 
@@ -30,7 +42,15 @@ resource "aws_cloudfront_distribution" "cdn" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-your-s3-bucket-name"
+    target_origin_id = "S3-stock-forex-app"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
@@ -47,4 +67,22 @@ resource "aws_cloudfront_distribution" "cdn" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+}
+# S3 Bucket Policy to restrict access to CloudFront OAI
+resource "aws_s3_bucket_policy" "static_files_policy" {
+  bucket = aws_s3_bucket.static_files.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+        }
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_files.arn}/*"
+      }
+    ]
+  })
 }
