c10261db52db953a0506d3b4da2cd1ecc570838b
Manan Bhimjiyani <mananbhimjiyani@gmail.com>
2025-04-08 16:21:54 +0530

    fix: CSS

--- stock_forex_app/settings.py
+++ stock_forex_app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", "FALSE") == "TRUE"  # Convert string to boolean
+DEBUG = os.environ.get("DEBUG", "FALSE") == "TRUE"  # Convert string to boolean
 
 DEBUG = True
 
