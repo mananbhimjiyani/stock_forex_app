d6e7190188307d2d901206bed413a973b9379055
Manan Bhimjiyani <mananbhimjiyani@gmail.com>
2025-04-08 16:13:24 +0530

    fix: ENV Variables

--- stock_forex_app/settings.py
+++ stock_forex_app/settings.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
+# from dotenv import load_dotenv
+#
+# load_dotenv()
 
 # Base Directory
 BASE_DIR = Path(__file__).resolve().parent.parent
